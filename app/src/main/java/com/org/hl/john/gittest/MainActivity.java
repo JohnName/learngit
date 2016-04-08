a50e665b3bf5863b70130c6cc2459ec4efc391f2
John <475934874@qq.com>
2016-04-08 17:59:44 +0800

    添加第二个跳转Activity测试onStop 和 trimMemory

--- app/src/main/java/com/org/hl/john/gittest/MainActivity.java
+++ app/src/main/java/com/org/hl/john/gittest/MainActivity.java
@@ -1,18 +1,36 @@
 package com.org.hl.john.gittest;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onTrimMemory(int level) {
+        super.onTrimMemory(level);
+        Log.i("AAAAA","隐藏！！");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("xxxxxxx","ssss  ");
+        Button button = (Button) findViewById(R.id.btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,SecondActivity.class));
+            }
+        });
+    }
 
-
-        Log.e("e","eeee");
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("BBBBBB","停止！");
     }
 }
---
+++ app/src/main/java/com/org/hl/john/gittest/SecondActivity.java
@@ -0,0 +1,15 @@
+package com.org.hl.john.gittest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by John on 2016/4/8.
+ */
+public class SecondActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second);
+    }
+}
